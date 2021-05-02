d89535400c5d2c891f6b266d5a668b1e32c0e4e6
abhishekv5055 <abhishek.vaish_int18@gla.ac.in>
2021-05-02 13:08:59 +0530

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import styled from "styled-components";
+
+const App = () => {
+  return (
+    <div>
+      <Home>Welcome to Discord Clone</Home>
+    </div>
+  );
+};
+
+export default App;
+
+const Home = styled.div`
+  margin: 0;
+  padding: 0;
+`;
